e68b1aef2ea734fe63b4f02034cfaf13bf9bf293
Julianoo <79302974+JayyyPro@users.noreply.github.com>
2024-12-08 18:50:52 +0100

    Update README.md with credentials setup and modify data retrieval loop in data.py

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
-## Prérequis
+### Prérequis
+Création d'un fichier credentials.json avec les informations minimum suivantes:
+```bash
+{
+    "API_KEY": "<YOUR_API_KEY>",
+    "LAT": YOUR_LATTITUDE,
+    "LON": YOUR_LONGITUDE
+}
+```
+Plus de paramètres sont ajoutables à cette liste.
+Les paramètres optionnels sont les suivants:
+```bash
+    "METRIC": "<YOUR_METRIC>",
+    "LANG": "<YOUR_LANGUAGE>"
+```
+Installation de dash et de pandas nécessaire à l'affichage des informations et graphiques:
 ```bash
 pip install dash
 pip install pandas
 ```
 
-## Utilisation de Python
 ### Récupération de données
 - Excellente prise en charge de la récupération de données depuis des bases de données (via pandas, SQLAlchemy, psycopg2, etc.).
 - Intégré avec des outils pour des API (REST/GraphQL) et le scraping web (requests, BeautifulSoup, Selenium).
--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 # Récupération des credentials
 with open("credentials.json", "r") as file:
@@ -8,20 +9,23 @@ with open("credentials.json", "r") as file:
 # URL de l'API
 api_url = f"https://api.openweathermap.org/data/2.5/weather?lat={credentials['LAT']}&lon={credentials['LON']}&appid={credentials['API_KEY']}&units={credentials['METRIC']}&lang={credentials['LANG']}"
 
-try:
-    # Envoi de la requête GET
-    response = requests.get(api_url)
+while True:
+    try:
+        # Envoi de la requête GET
+        response = requests.get(api_url)
 
-    # Vérification du statut de la réponse
-    if response.status_code == 200:
-        # Conversion de la réponse JSON en dictionnaire Python
-        data = response.json()
-        # Sauvegarde des données dans un fichier JSON
-        with open("data.json", "w", encoding="utf-8") as file:
-            json.dump(data, file, ensure_ascii=False, indent=4)
-        print("Données sauvegardées dans le fichier data.json")
-    else:
-        print(f"Erreur {response.status_code} : {response.text}")
-
-except requests.exceptions.RequestException as e:
-    print("Une erreur est survenue :", e)
+        # Vérification du statut de la réponse
+        if response.status_code == 200:
+            # Conversion de la réponse JSON en dictionnaire Python
+            data = response.json()
+            # Affichage des données
+            print(data)
+            # Sauvegarde des données dans un fichier JSON
+            with open("data.json", "w", encoding="utf-8") as file:
+                json.dump(data, file, ensure_ascii=False, indent=4)
+            print("Données sauvegardées dans le fichier data.json")
+        else:
+            print(f"Erreur {response.status_code} : {response.text}")
+    except requests.exceptions.RequestException as e:
+        print("Une erreur est survenue :", e)
+    time.sleep(90)  # Attente de 60 secondes
