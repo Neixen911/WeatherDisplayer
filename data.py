05ef5dac26a04ab9d4874a1eca939ce3f34af5b7
Julianoo <79302974+JayyyPro@users.noreply.github.com>
2025-01-01 21:05:33 +0100

    Refactor weather data retrieval in data.py and add HTML template for display

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, stream_with_context
+from flask import Flask, render_template
 import requests
 import json
 import time
@@ -12,54 +12,57 @@ with open("credentials.json", "r") as file:
 
 # URL de l'API
 current_weather = f"https://api.openweathermap.org/data/2.5/weather?lat={credentials['LAT']}&lon={credentials['LON']}&appid={credentials['API_KEY']}&units={credentials['METRIC']}&lang={credentials['LANG']}"
-three_hour_forecast= f"https://api.openweathermap.org/data/2.5/forecast?lat={credentials['LAT']}&lon={credentials['LON']}&appid={credentials['API_KEY']}&units={credentials['METRIC']}&lang={credentials['LANG']}"
+three_hour_forecast= f"https://api.openweathermap.org/data/2.5/forecast/daily?lat={credentials['LAT']}&lon={credentials['LON']}&cnt={7}&appid={credentials['API_KEY']}&units={credentials['METRIC']}&lang={credentials['LANG']}"
 
 def get_weather():
+    weather_informations = {
+        'current-weather': {}, 
+        'daily-forecast': []
+    }
     while True:
         try:
             # Current weather
             response = requests.get(current_weather)
             if response.status_code == 200:
                 data = response.json()
-                yield f"Icone: {data['weather'][0]['icon']}\n"
-                yield f"Temps: {data['weather'][0]['main']}\n"
-                yield f"Description: {data['weather'][0]['description']}\n"
-                yield f"Température: {data['main']['temp']}°C\n"
-                yield f"Ressenti: {data['main']['feels_like']}°C\n"
-                yield f"Min: {data['main']['temp_min']}°C\n"
-                yield f"Max: {data['main']['temp_max']}°C\n"
-                yield f"Vent: {data['wind']['speed']} m/s\n"
-                yield f"Levé du soleil: {time.strftime('%H:%M', time.gmtime(data['sys']['sunrise']))}\n"
-                yield f"Couché du soleil: {time.strftime('%H:%M', time.gmtime(data['sys']['sunset']))}\n"
-                # yield f"{data}\n"
-                yield f"\n"
+                weather_informations['current-weather']['icon'] = data['weather'][0]['icon']
+                weather_informations['current-weather']['main'] = data['weather'][0]['main']
+                weather_informations['current-weather']['description'] = data['weather'][0]['description']
+                weather_informations['current-weather']['temp'] = data['main']['temp']
+                weather_informations['current-weather']['feels_like'] = data['main']['feels_like']
+                weather_informations['current-weather']['temp_min'] = data['main']['temp_min']
+                weather_informations['current-weather']['temp_max'] = data['main']['temp_max']
+                weather_informations['current-weather']['wind'] = data['wind']['speed']
+                weather_informations['current-weather']['sunrise'] = time.strftime('%H:%M', time.gmtime(data['sys']['sunrise']))
+                weather_informations['current-weather']['sunset'] = time.strftime('%H:%M', time.gmtime(data['sys']['sunset']))
             else:
-                yield f"Erreur {response.status_code} : {response.text}"
+                return f"Erreur {response.status_code} : {response.text}"
 
-            # 3-hour forecast for 5 days
+            # Daily forecast on 7 days
             response = requests.get(three_hour_forecast)
             if response.status_code == 200:
                 data = response.json()
                 # Itère sur chaque élément de la liste data['list']
                 for i in range(0, len(data['list'])):
-                    yield f"Horaires de prévision: {data['list'][i]['dt_txt']}\n"
-                    yield f"Icone: {data['list'][i]['weather'][0]['icon']}\n"
-                    yield f"Temps: {data['list'][i]['weather'][0]['main']}\n"
-                    yield f"Description: {data['list'][i]['weather'][0]['description']}\n"
-                    yield f"Température: {data['list'][i]['main']['temp']}°C\n"
-                # yield f"{data['list'][0]}\n"
-                # yield f"{data}\n"
-                yield f"\n"
+                    day_forecast = {
+                        'icon': data['list'][i]['weather'][0]['icon'],
+                        'main': data['list'][i]['weather'][0]['main'],
+                        'description': data['list'][i]['weather'][0]['description'],
+                        'temp_min': data['list'][i]['temp']['min'],
+                        'temp_max': data['list'][i]['temp']['max']
+                    }
+                    weather_informations['daily-forecast'].append(day_forecast)
             else:
-                yield f"Erreur {response.status_code} : {response.text}"
+                return f"Erreur {response.status_code} : {response.text}"
         except requests.exceptions.RequestException as e:
             print(f"Erreur : {e}")
 
         time.sleep(90)  # Attente de 90 secondes
+        return weather_informations
 
 @app.route('/')
 def weather():
-    return Response(stream_with_context(get_weather()), mimetype='text/event-stream')
+    return render_template('app.html', weather=get_weather())
 
 if __name__ == "__main__":
     app.run(debug=True)
